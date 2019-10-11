70f871e24e88470da8565136bdaf23948081cd8f
Huda Yousif <hjdoski@gmail.com>
2019-10-11 05:31:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,24 +1,34 @@
+#1
 def sort_array_asc(array)
   array.sort do |a, b|
     a <=> b 
   end
 end 
 
+#2
 def sort_array_desc(array)
   array.sort do |a, b|
     b <=> a 
   end
 end
 
+#3
 def sort_array_char_count(array)
   array.sort { |a, b| a.length <=> b.length }
 end
 
+#4
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
+#5
 def reverse_array(array)
   array.reverse
+end
+
+#6
+def kesha_maker(array)
+  
 end
